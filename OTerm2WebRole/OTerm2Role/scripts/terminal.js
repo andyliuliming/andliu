e8e20e9ad39be96fc569b372f68ce63155c2fc53
andyliuliming <andyliuliming@outlook.com>
2015-05-15 16:55:14 +0800

    xx

--- OTerm2WebRole/OTerm2Role/scripts/terminal.js
+++ OTerm2WebRole/OTerm2Role/scripts/terminal.js
@@ -1,10 +1,11 @@
+function renderTerminal() {
 
-
-term = new Terminal({
-                cols: 120,
-                rows: 60,
-                useStyle: true,
-                screenKeys: true,
-                cursorBlink: false
-            });
-            term.open(document.body);
+    term = new Terminal({
+        cols: 120,
+        rows: 60,
+        useStyle: true,
+        screenKeys: true,
+        cursorBlink: false
+    });
+    term.open(document.body);
+}
