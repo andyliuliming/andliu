25755f33ec32507399dbf48da26d3952362ee656
andyliuliming <andyliuliming@gmail.com>
2015-05-16 21:39:51 +0800

    shoo

---
+++ OTerm2/src/ostc/sh/webconsole/config/Palette.java
@@ -0,0 +1,13 @@
+package ostc.sh.webconsole.config;
+
+import java.awt.Color;
+
+public class Palette {
+	public static Color ButtonSelectedBackground=new Color(0, 153, 255);
+	public static Color ButtonSelectedForColor=Color.WHITE;
+	public static Color ButtonNormalBackground=Color.WHITE;
+	public static Color MainPageBackground=new Color(46,127,170);
+	public static Color LightBackground=new Color(31,31,31);
+	public static Color ForeColor = new Color(155, 155, 155);
+	public static Color MiddleDarkColor = new Color(117, 117, 117);
+}
