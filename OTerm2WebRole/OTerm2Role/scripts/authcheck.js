25d2b585bc02e7ad3145a22550a9f7e0b740d560
andyliuliming <andyliuliming@outlook.com>
2015-05-18 08:47:06 +0800

    sxxx

--- OTerm2WebRole/OTerm2Role/scripts/authcheck.js
+++ OTerm2WebRole/OTerm2Role/scripts/authcheck.js
@@ -3,11 +3,22 @@
     if (idToken != null) {
         var decodedString = atob(idToken.split('.')[1]);
         var id = JSON.parse(decodedString);
-        if (id.email.endsWith("@microsoft.com")) {
-            return true;
+        if (id.unique_name != null) {
+            if (id.unique_name.endsWith("@microsoft.com")) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
-        else {
-            return false;
+
+        if (id.email != null) {
+            if (id.email.endsWith("@microsoft.com")) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     } else {
         window.location = "https://login.windows.net/common/oauth2/authorize?response_type=id_token&client_id=b3a1287b-34cc-45b1-861e-da08d7ea0cfc&scope=openid&nonce=7362CAEA-9CA5-4B43-9BA3-34D7C303EBA7&response_mode=query";
