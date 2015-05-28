16c19f245db44776feaecf76ea7e37440e13d7f3
andyliuliming <andyliuliming@outlook.com>
2015-05-28 16:05:44 +0800

    fix the focus bug

--- AzureTerminal/AzureTerminalAzureService/AzureTerminalWebRole/scripts/applet.js
+++ AzureTerminal/AzureTerminalAzureService/AzureTerminalWebRole/scripts/applet.js
@@ -1,16 +1,17 @@
 function initializeApplet() {
-	var attributes = {
-		id : 'otermApplet',
-		code : 'ostc.sh.webconsole.OTermApplet.class',
-		width : '0',
-		height : '0',
-		margin : '0'
-	};
+    var attributes = {
+        id: 'otermApplet',
+        code: 'ostc.sh.webconsole.OTermApplet.class',
+        width: '0',
+        height: '0',
+        margin: '0'
+    };
 
-	var parameters = {
-		jnlp_href : './java/WebConsole.jnlp'
-	};
+    var parameters = {
+        jnlp_href: './java/WebConsole.jnlp',
+        initial_focus: 'false'
+    };
 
-	deployJava.runApplet(attributes, parameters, '1.7');
+    deployJava.runApplet(attributes, parameters, '1.7');
 
 }
