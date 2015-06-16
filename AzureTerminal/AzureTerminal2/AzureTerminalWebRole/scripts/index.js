dd5c978cbc3ff403ea86f3c7a0c291ed00c083e2
andyliuliming <andyliuliming@outlook.com>
2015-06-16 15:48:35 +0800

    little adjustment.

--- AzureTerminal/AzureTerminal2/AzureTerminalWebRole/scripts/index.js
+++ AzureTerminal/AzureTerminal2/AzureTerminalWebRole/scripts/index.js
@@ -21,8 +21,8 @@ function getLocalPathSeperator() {
     }
 }
 function useHttps() {
-    //if (window.location.protocol != "https:")
-    //    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+    if (window.location.protocol != "https:")
+        window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 }
 
 function switchToTerminalMainPage() {
