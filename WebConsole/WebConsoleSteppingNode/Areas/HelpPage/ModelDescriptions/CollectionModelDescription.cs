39d0569280629bb9e7fa2d340bce14170aedb252
andyliuliming <andyliuliming@outlook.com>
2015-06-18 13:45:33 +0800

    ttttt

--- WebConsole/WebConsoleSteppingNode/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace WebConsoleSteppingNode.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- WebConsole/WebConsoleSteppingNode/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebConsoleSteppingNode.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
