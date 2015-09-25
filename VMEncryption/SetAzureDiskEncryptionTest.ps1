692e7c1330fb887227b7fce80f93688b98d33501
andyliuliming <andyliuliming@outlook.com>
2015-09-25 13:27:55 +0800

    change name...

--- VMEncryption/SetAzureDiskEncryptionTest.ps1
+++ VMEncryption/SetAzureDiskEncryptionTest.ps1
@@ -10,6 +10,58 @@ Set-AzureKeyVaultAccessPolicy -VaultName andliukeyvault -ResourceGroupName andli
 
 Add-AzureKeyVaultKey -VaultName "andliukeyvault" -Name "mykey" -Destination Software
 
+
+
+
+
+
+$ResourceGroupName = "diskencryptiontst"
+$Location = "westus"
+
+## Storage
+$StorageName = "andliuencrypt"
+$StorageType = "Standard_GRS"
+
+## Network
+$InterfaceName = "andliuencryptifn"
+$Subnet1Name = "Subnet1"
+$VNetName = "VNet09"
+$VNetAddressPrefix = "10.0.0.0/16"
+$VNetSubnetAddressPrefix = "10.0.0.0/24"
+
+## Compute
+$VMName = "ubuntu-encrypt"
+$ComputerName = "Server22"
+$VMSize = "Standard_A2"
+$OSDiskName = $VMName + "osDisk"
+
+# Resource Group
+New-AzureResourceGroup -Name $ResourceGroupName -Location $Location
+
+# Storage
+$StorageAccount = New-AzureStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageName -Type $StorageType -Location $Location
+
+# Network
+$PIp = New-AzurePublicIpAddress -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Dynamic
+$SubnetConfig = New-AzureVirtualNetworkSubnetConfig -Name $Subnet1Name -AddressPrefix $VNetSubnetAddressPrefix
+$VNet = New-AzureVirtualNetwork -Name $VNetName -ResourceGroupName $ResourceGroupName -Location $Location -AddressPrefix $VNetAddressPrefix -Subnet $SubnetConfig
+$Interface = New-AzureNetworkInterface -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[0].Id -PublicIpAddressId $PIp.Id
+
+# Compute
+
+## Setup local VM object
+$Credential = Get-Credential
+$VirtualMachine = New-AzureVMConfig -VMName $VMName -VMSize $VMSize
+$VirtualMachine = Set-AzureVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Set-AzureVMSourceImage -VM $VirtualMachine -PublisherName AzureRT.PIRCore.TestWAStage -Offer TestUbuntuServer -Skus 14.10 -Version "latest"
+$VirtualMachine = Add-AzureVMNetworkInterface -VM $VirtualMachine -Id $Interface.Id
+$OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + "vhds/" + $OSDiskName + ".vhd"
+$VirtualMachine = Set-AzureVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -CreateOption FromImage
+
+## Create the VM in Azure
+New-AzureVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine
+
+
 #"command":"enableencryption_all_inplace",
 #"KeyEncryptionKeyURL":"https://andliukeyvault.vault.azure.net/keys/mykey",
 #        "KeyVaultURL":"https://andliukeyvault.vault.azure.net/",
