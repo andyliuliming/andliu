1492d2ddf96f75197d65fbe843625bd145c9fa2f
andyliuliming <andyliuliming@outlook.com>
2015-10-30 23:10:50 +0800

    $keyEncryptionKeyVaultId

--- VMEncryption/SetAzureDiskEncryptionTest.ps1
+++ VMEncryption/SetAzureDiskEncryptionTest.ps1
@@ -7,16 +7,17 @@ Add-AzureRmAccount
 Set-AzureRmContext -SubscriptionName "OSTC Shanghai Dev"
 
 $resourceGroupName="andliuresourcegroup2"
-$vmName="andliu-encrypt3"
+$vmName="andliu-encrypt2"
 $aadClientId="b7b48143-6c58-4cd4-a9e0-0a15cbda0614"
 $aadClientSecret="/XazYdJ9XaMBbiQ0dwSoyue7LbkQ1OJOePGGcrG3dkA="
 $keyVaultUrl="https://andliukeyvault.vault.azure.net"
 #$location="North Central US"
 $diskEncryptionKeyVaultId="/subscriptions/c4528d9e-c99a-48bb-b12d-fde2176a43b8/resourceGroups/andliuresourcegroup2/providers/Microsoft.KeyVault/vaults/andliukeyvault"
+$keyEncryptionKeyVaultId="/subscriptions/c4528d9e-c99a-48bb-b12d-fde2176a43b8/resourceGroups/andliuresourcegroup2/providers/Microsoft.KeyVault/vaults/andliukeyvault"
 $keyEncryptionKeyUrl="https://andliukeyvault.vault.azure.net/keys/andliukeyvaultkek/a86443419dfa4d7e9ebdf3189e9677fb"
 $keyEncryptionAlgorithm="RSA-OAEP"
 $volumeType="Data"
-$extensionName="andliu-encrypt3"
+$extensionName="andliu-encrypt2"
 
 
 #"command":"enableencryption_all_inplace",
@@ -25,7 +26,7 @@ $extensionName="andliu-encrypt3"
 #        "AADClientID":"b7b48143-6c58-4cd4-a9e0-0a15cbda0614",
 #        "KeyEncryptionAlgorithm":"RSA1_5",
 #        "BitlockerVolumeType":"Data"
-Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $resourceGroupName -VMName $vmName -AadClientID $aadClientId -AadClientSecret $aadClientSecret -DiskEncryptionKeyVaultUrl $keyVaultUrl -DiskEncryptionKeyVaultId $diskEncryptionKeyVaultId -KeyEncryptionKeyUrl $keyEncryptionKeyUrl -KeyEncryptionAlgorithm $keyEncryptionAlgorithm -VolumeType $volumeType -Name $extensionName
+Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $resourceGroupName -VMName $vmName -AadClientID $aadClientId -AadClientSecret $aadClientSecret -DiskEncryptionKeyVaultUrl $keyVaultUrl -DiskEncryptionKeyVaultId $diskEncryptionKeyVaultId -KeyEncryptionKeyUrl $keyEncryptionKeyUrl -KeyEncryptionKeyVaultId $keyEncryptionKeyVaultId -KeyEncryptionAlgorithm $keyEncryptionAlgorithm -VolumeType $volumeType -Name $extensionName
 
 
 azure vm enable-disk-encryption -g andliuresourcegroup2 -n andliu-encrypt -a b7b48143-6c58-4cd4-a9e0-0a15cbda0614 -p /XazYdJ9XaMBbiQ0dwSoyue7LbkQ1OJOePGGcrG3dkA= -k https://andliukeyvault.vault.azure.net -r /subscriptions/c4528d9e-c99a-48bb-b12d-fde2176a43b8/resourceGroups/andliuresourcegroup2/providers/Microsoft.KeyVault/vaults/andliukeyvault -t Data
