67bccec88209a2d36bb0a8ff9bbefc1e6d4e58e3
andyliuliming <andyliuliming@outlook.com>
2016-04-09 12:48:05 +0800

    script update.

--- TalentGraber/AutoDeploy/pslib/make_resources.ps1
+++ TalentGraber/AutoDeploy/pslib/make_resources.ps1
@@ -12,27 +12,14 @@ $ErrorActionPreference= 'Continue'
 MakeSureAffinityGroupExists -Location $location -AffinityGroupName $AffinityGroupName
 # create the cloud services if not exists
 MakeSureAzureServiceExists -ServiceName $ProductAzureServiceName -AffinityGroupName $AffinityGroupName
-MakeSureAzureServiceExists -ServiceName $StarDustPortalAzureServiceName -AffinityGroupName $AffinityGroupName
-
 
 MakeSureResourceGroupExists -ResourceGroupName $ResourceGroupName -Location $location
-$Sku = "Basic"
-$Size = "C2"
-MakeSureRedisCacheExists -RedisCacheServerName $StarDustRedisCacheProductServerName -ResourceGroupName $ResourceGroupName -Location $location -Size $Size -Sku $Sku
-
 
 $databaseSecpasswd = ConvertTo-SecureString $databasePassword -AsPlainText -Force
 $databaseCredential = New-Object System.Management.Automation.PSCredential ($databaseUserName, $databaseSecpasswd)
 MakeSureDatabaseServerExists -ResourceGroupName $ResourceGroupName -ServerName $databaseServerName -Credential $databaseCredential -Location $location
 MakeSureDatabaseExists -ResourceGroupName $ResourceGroupName -DatabaseName $goldDataBaseName -ServerName $databaseServerName -DatabaseEdition $databaseEdition -DatabaseMaxSize $databaseMaxSize
 
-
 MakeSureStorageAccountExists -StorageName $diagnosticStorageAccountName -Location $location
 MakeSureStorageAccountExists -StorageName $deploymentStorageAccountName -Location $location
 MakeSureStorageAccountExists -StorageName $imageStorageAccountName -Location $location
-
-
-#MakeSureReservedIpExists -ReservedIpName $reservedIpForProductServiceName -Location $location
-#MakeSureReservedIpExists -ReservedIpName $reservedIpForProductServiceNameStage -Location $location
-#MakeSureReservedIpExists -ReservedIpName $reservedIpForPortalServiceName -Location $location
-#MakeSureReservedIpExists -ReservedIpName $reservedIpForPortalServiceNameStage -Location $location
