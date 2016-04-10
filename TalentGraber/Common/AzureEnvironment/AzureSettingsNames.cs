e66e776c96c79e3361664e66f621cb4e7c8e0f8e
andyliuliming <andyliuliming@outlook.com>
2016-04-10 22:02:00 +0800

    .

--- TalentGraber/Common/AzureEnvironment/AzureSettingsNames.cs
+++ TalentGraber/Common/AzureEnvironment/AzureSettingsNames.cs
@@ -4,11 +4,7 @@
     public class AzureSettingsNames
     {
         public string AllowedCrossDomainHost { get; set; }
-        public string StarDustProductServiceAddress { get; set; }
-        public string StarDustProductCacheAddress { get; set; }
-        public string StarDustProductCacheKey { get; set; }
-        public string WanXiangAppId { get; set; }
-        public string WanXiangSecretId { get; set; }
-        public string WanXiangSecretKey { get; set; }
+        public string ProductServiceAddress { get; set; }
+        public string ProductCacheAddress { get; set; }
     }
 }
