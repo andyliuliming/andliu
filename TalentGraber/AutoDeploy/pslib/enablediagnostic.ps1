ae885543eb7df133b4155b4a2fba0454d783e257
andyliuliming <andyliuliming@outlook.com>
2016-04-10 21:02:22 +0800

    enable diagnostic.

--- TalentGraber/AutoDeploy/pslib/enablediagnostic.ps1
+++ TalentGraber/AutoDeploy/pslib/enablediagnostic.ps1
@@ -11,11 +11,11 @@ Write-Host "DiagnosticStorageAccountName is $diagnosticStorageAccountName "
 $diagnosticStorageKey=GetStorageAccountKey -ResourceGroupName $ResourceGroupName -StorageAccountName $diagnosticStorageAccountName
 $diagnosticStorageContext = New-AzureStorageContext -StorageAccountName $diagnosticStorageAccountName -StorageAccountKey $diagnosticStorageKey
 
-$ProductServiceDiagnosticConfig = "$currentFolder/$ProductAzureServiceName/bin/$buildType/app.publish/Extensions/PaaSDiagnostics.$ProductServiceRoleName.PubConfig.xml"
+$ProductServiceDiagnosticConfig = "$currentFolder/$ProductAzureServiceProjectName/bin/$buildType/app.publish/Extensions/PaaSDiagnostics.$ProductServiceRoleName.PubConfig.xml"
 $diagnosticConfig = Get-Content -Encoding UTF8 -Path $ProductServiceDiagnosticConfig
 $diagnosticConfig = ReplaceRealValue -TemplateOrigin $diagnosticConfig
 Set-Content -Encoding UTF8 -Path $ProductServiceDiagnosticConfig -Value $diagnosticConfig
 
 Write-Host "setting the product service diagnostic extension"
-SetDiagnosticExtension -ServiceName $ProductAzureServiceName -StorageContext $diagnosticStorageContext -RoleName $ProductServiceRoleName -DiagnosticConfigurationPath $StarDustProductServiceDiagnosticConfig -Slot Production
+SetDiagnosticExtension -ServiceName $ProductAzureServiceName -StorageContext $diagnosticStorageContext -RoleName $ProductServiceRoleName -DiagnosticConfigurationPath $ProductServiceDiagnosticConfig -Slot Production
 
