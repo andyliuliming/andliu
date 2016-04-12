48d03bdd1919cd4bf9ca63cc6ef0b91b76bf40d9
andyliuliming <andyliuliming@outlook.com>
2016-04-12 22:56:50 +0800

    error message improvement.

--- TalentGraber/ProductService/manage/githubrepo/githubrepo-add.js
+++ TalentGraber/ProductService/manage/githubrepo/githubrepo-add.js
@@ -12,32 +12,36 @@
     var tmpl = require("text!manage/githubrepo/githubrepo-add.html");
     util.loadTmpl(tmplId, tmpl);
 
-    githubRepoAdd.GithubRepoAddViewModel = function (isAdd) {
+    githubRepoAdd.GithubRepoAddViewModel = function () {
         var self = this;
-        self.loading = ko.observable(true);
+        self.loading = ko.observable(false);
+        self.error = ko.observable("");
+        self.enableSave = ko.observable(false);
         self.error = ko.observable("");
         self.githubRepo = komapping.fromJS({
             Id: 0,
             Url: ""
         });
 
-        self.loading(false);
+        self.enableSave(true);
     };
 
     githubRepoAdd.GithubRepoAddViewModel.prototype.saveGithubRepo = function () {
         console.dir("save github repo");
         var self = this;
+        self.enableSave(false);
         var githubRepoObj = komapping.toJS(self.githubRepo);
         util.ajax({
             url: util.productEndpoint + "odata/GithubRepoes",
             type: "POST",
             data: JSON.stringify(githubRepoObj),
             success: function (data) {
-                if (!data) {
-                    alert("Failed to create github repo.");
-                }
+                self.enableSave(true);
+                self.error("Add sucessfually.");
             },
             error: function (e) {
+                self.enableSave(true);
+                self.error("Add failed, Please try again later.");
                 console.log("Failed to submit github repo code: " + e.status);
             }
         });
